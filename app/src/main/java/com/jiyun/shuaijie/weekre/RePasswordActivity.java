0de57180448a8eee0a7470aabe93c19e72892ffa
shuaiJie <13838469758@qq.com>
2017-09-12 15:56:34 +0800

    测试提交 HomeActivity是首页 RegisterActivity是注册 RePasswordActivity是找回密码 LoginActivity是登录 MainActivity是启动页

---
+++ app/src/main/java/com/jiyun/shuaijie/weekre/RePasswordActivity.java
@@ -0,0 +1,13 @@
+package com.jiyun.shuaijie.weekre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_re_password);
+    }
+}
